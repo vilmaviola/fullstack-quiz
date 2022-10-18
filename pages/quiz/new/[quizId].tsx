5086476ab01da4e4c9689b6a52bd885b9efe658e
Vilma Ahlholm <vilma.ahlholm@northvolt.com>
2022-10-18 14:54:18 +0200

    Dynamic addition of answers to question and Bulk insert answers to table

--- pages/quiz/new/[quizId].tsx
+++ pages/quiz/new/[quizId].tsx
@@ -1,15 +1,18 @@
-import { User } from '@supabase/supabase-js'
-import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { FormEvent, useEffect, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { supabase } from '../../../utils/supabaseClient'
 import Layout from '../../Layout'
 
+type Answer = {
+  answer?: string,
+  correct?: boolean,
+}
 
 function AddQuestions() {
 
   const [question, setQuestion] = useState('')
-  const [option1, setOption1] = useState('')
+
+  const [options, setOptions] = useState<Answer[]>([{ answer: '', correct: false }])
 
   const router = useRouter()
   const { quizId } = router.query
@@ -25,28 +28,70 @@ function AddQuestions() {
               quizId: quizId
             }
         ).select().limit(1).single()
+    
+      const bulkOption = options.map((option) => (
+          {...option, questionId: data.id}
+      ))
 
     const { data: answers, error: answersError } = await supabase
     .from('answers')
-    .insert(
-        { 
-          answer:  option1,
-          correct: true,
-          questionId: data.id,
-        }
-    ).select().limit(1).single()
+    .insert(bulkOption)
 
+    if (!error && !answersError) {
+      alert('Question added')
+      setQuestion('')
+      setOptions([{answer: '', correct: false}])
+    } else if (error || answersError) {
+      alert('Something went wrong. Could not add question.')
+    }
+}
+
+function handleChange(event: ChangeEvent<HTMLInputElement>, index: number) {
+  const value = event.target.value
+  setOptions((previousArray) => {
+    const update = [...previousArray]
+    const updateOption = update[index]
+
+    update[index] = {...updateOption, answer: value}
+    return update
+  })
+}
+
+function handleChecked(event: ChangeEvent<HTMLInputElement>, index:number) {
+  const checked = event.target.checked
+
+  setOptions((previousArray) => {
+    const update = [...previousArray]
+    const updateOption = update[index]
+
+    update[index] = {...updateOption, correct: checked}
+    return update
+  })
 }
 
   return (
     <Layout>
         <div>
             <h1>Add questions here!</h1>
-              <form onSubmit={addQuestion}>
+            <br></br><br></br>
+
+              <form onSubmit={addQuestion} >
                   <label>Question</label><br></br>
-                  <input type='text' placeholder='Type the question' onChange={(event) => setQuestion(event.target?.value)}/><br></br>
-                  <input type='text' placeholder='Type option 1' onChange={(event) => setOption1(event.target?.value)}/><br></br>
-                  <input type="submit" value="Add question"/>
+                  <input type='text' placeholder='Type the question' value={question} onChange={(event) => setQuestion(event.target?.value)}/><br></br>
+                  <br></br>
+
+              <br></br><br></br>
+                {options.map((option, index) => (
+                  <>
+                    <input key={index} value={option.answer} placeholder='Enter answer' onChange={(event) => handleChange(event, index)}></input>
+                    <input type='checkbox' checked={option?.correct} onChange={(event) => handleChecked(event, index)} />
+                    <br></br>
+                  </>
+                ))}
+              <br></br><br></br>
+                <input type='button' value={'Add answer'} onClick={() => setOptions((previousAnswers) => [...previousAnswers, { answer: '', correct: false }])}></input>
+                <br></br><br></br>
+                <input type="submit" value="Add question"/>
               </form>
         </div>
     </Layout>
